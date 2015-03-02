79000362ac214d4e88e106b8e2591a8cfc47159b
Josjs <Josjs@users.noreply.github.com>
2015-03-02 23:11:33 +0100

    New and old code, uses new
    Added timecode, precise delay etc.

--- CanSat.ino
+++ CanSat.ino
@@ -1,31 +1,41 @@
 #include <SoftwareSerial.h>
 
+#define nsens 5 //Needed to construct the string
+
 //Define ALL the pins
-int temppin = 1;
-int trykkpin = 2;
-int axelxpin = 3;
-int axelypin = 4;
-int axelzpin = 5;
+int temppin = A5;  //temperature
+int trykkpin = A1; //pressure
+int axelxpin = A2; //axelerometer x
+int axelypin = A3; //axelerometer y
+int axelzpin = A4; //axelerometer z
 
 //Make an extra serial shiz for OpenLog, in case we need it
-SoftwareSerial OpenLog(3, 2);
+//Edit: We don't
+SoftwareSerial OpenLog(1, 0);
 
-int rrate = 555; //delay in m
+int rrate = 200; //delay in ms
+long timecode = 0;
 
 void setup() {
 OpenLog.begin(9600);
 Serial.begin(9600);
-delay(1000); //Saw a guy on the internet do this
+//delay(1000); //Saw a guy on the internet do this
+while (millis() < 1000);
 }
 
 void loop() {
+timecode = millis();  
+  
 int temp = analogRead(temppin);
 int trykk = analogRead(trykkpin);
 int ax = analogRead(axelxpin);
 int ay = analogRead(axelypin);
 int az = analogRead(axelzpin);  
-//OpenLog.println(temp, trykk, ax, ay, az);
-OpenLog.print(temp);
+
+int sensor[nsens] = {temp, trykk, ax, ay, az};
+
+//failsafe method
+/*Serial.print(temp);
 Serial.print(", ");
 Serial.print(trykk);
 Serial.print(", ");
@@ -35,7 +45,22 @@ Serial.print(ay);
 Serial.print(", ");
 Serial.print(az);
 Serial.print("\r\n");
-delay(rrate);
+*/
+
+//new fancy shit
+//String sensordata = String(temp) + String(" ") + String(trykk) + String(" ") + String(ax) + String(" ") + String(ay) + String(" ") + String(az);
+String sensordata = String(timecode);
+
+for (int i = 0; i < nsens; i++){
+  sensordata.concat(", ");
+  sensordata.concat(sensor[i]);
+}
+
+
+Serial.println(sensordata);
+
+while (millis() < timecode + rrate);
+//delay(rrate);
 }
 
 /*int sensordata(){
