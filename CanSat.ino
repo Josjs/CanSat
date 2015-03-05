50665e9bf1ef74a15de23d9808e9ad6c89eeecb2
Josjs <Josjs@users.noreply.github.com>
2015-03-05 23:19:24 +0100

    Cleanup

--- CanSat.ino
+++ CanSat.ino
@@ -11,6 +11,7 @@ int axelzpin = A4; //axelerometer z
 
 int rrate = 200; //delay in ms
 long timecode = 0;
+bool raw = 1;
 
 void setup() {
   Serial.begin(9600);
@@ -20,20 +21,33 @@ void setup() {
 void loop() {
   timecode = millis();  
 
-  int temp = analogRead(temppin);
-  int trykk = analogRead(trykkpin);
-  int ax = analogRead(axelxpin);
-  int ay = analogRead(axelypin);
-  int az = analogRead(axelzpin);  
-
-  int sensor[nsens] = {temp, trykk, ax, ay, az};
-
+  if (raw = true){
+    int temp = analogRead(temppin);
+    int trykk = analogRead(trykkpin);
+    int ax = analogRead(axelxpin);
+    int ay = analogRead(axelypin);
+    int az = analogRead(axelzpin);  
+    int sensor[nsens] = {temp, trykk, ax, ay, az};
+  } else {
+      float temp = analogRead(temppin);
+      float trykk = analogRead(trykkpin);
+      float ax = analogRead(axelxpin);
+      float ay = analogRead(axelypin);
+      float az = analogRead(axelzpin);
+      float sensorf[nsens] = {temp, trykk, ax, ay, az};
+  }  
 //new fancy shit
   String sensordata = String(timecode);
 
   for (int i = 0; i < nsens; i++){
     sensordata.concat(", ");
-    sensordata.concat(sensor[i]);
+    if (raw = true){
+      sensordata.concat(sensor[i]);
+    } else{
+        sensordata.concat(sensorf[i]);
+      }
+
+    } 
 }
 
   Serial.println(sensordata);
